de838a58b19e8dd150813d534d3b77782f136a76
Jaroenta <pamjaroenta@gmail.com>
2018-05-30 16:20:32 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
-from flask import Flask, request ,abort
-from linebot import (LineBotApi, WebhookHanndler)
+from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 app = Flask(__name__)
-
 
 line_bot_api = LineBotApi('y5rsb056kL8UVgzoW7Vz8wE8dtmNPKSkXNO01/yddVDpIhmoCI9Sn53L0KZf/JdmJwFwQ5iAnhnJTAgjE6jUxwtlKjbS5FNwG0BxxBUH7FCnKa5YqDzU0nZSXd42chGB1LSZ/pm8IzABHdcv2Mv/AAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHanndler('09476e50209f395bfa593c7bd53cdef0')
-
-app = Flask(__name__)
  
-@app.route('/')
-def index():
-    return 'hello this is your server.'
 
-@app.route('/webhook', methods=['POST'])
+@app.route("/")
+def hello():
+    return "this is chatbot sever!"
 
+@app.route("/webhook", methods=['POST'])
 def webhook():
-    # get ค่า ผู้ใช้
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-   
+
     # get request body as text
-     body = request.get_date(as_text=True)
-     app.logger.info("Request body: "+body)
-    
-    
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
     try:
-        handler.handle(body, sinature)
-    except InvalidSignatureError
+        handler.handle(body, signature)
+    except InvalidSignatureError:
         abort(400)
-    return 'ok'
+
+    return 'OK'
+    
 
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event)
