71a4d2bb18ee4cf856ce78712947e7d364eca0a4
Jaroenta <pamjaroenta@gmail.com>
2018-05-30 16:44:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('y5rsb056kL8UVgzoW7Vz8wE8dtmNPKSkXNO01/yddVDpIhmoCI9Sn53L0KZf/JdmJwFwQ5iAnhnJTAgjE6jUxwtlKjbS5FNwG0BxxBUH7FCnKa5YqDzU0nZSXd42chGB1LSZ/pm8IzABHdcv2Mv/AAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHanndler('09476e50209f395bfa593c7bd53cdef0')
- 
+handler = WebhookHandler('09476e50209f395bfa593c7bd53cdef0')
+
+
 
 @app.route("/")
 def hello():
@@ -31,10 +32,70 @@ def webhook():
     return 'OK'
     
 
-@handler.add(MessageEvent,message=TextMessage)
-def handle_message(event)
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=evet.message.text))
- 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    
+    if event.message.text == 'สวัสดี':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='สวัสดีค่า'))
+        return 0
+    if event.message.text == "หวัดดี":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='หวัดดีค่ะ'))
+        return 0
+    if event.message.text == "สบายดีมั้ย":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='สบายดีค่า'))
+        return 0
+    if event.message.text == "หิวอะ":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='หิวก็หาอะไรกินสิ'))
+        return 0
+    if event.message.text == "เหงา":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='เหงาก็คุยกับเราก่อน'))
+        return 0
+    if event.message.text == "ทำไรอยู่":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='คุยกะเธอไง'))
+        return 0
+    if event.message.text == "แชทบอทคืออะไร":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='แชทบอทก็คือ โปรแกรมที่ตอบกลับข้อความกลับอัตโนมัติยังไงหล่ะ'))
+        return 0
+    if event.message.text == 'ชื่ออะไร':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='ชื่อแพมค่ะ'))
+        return 0
+    if event.message.text == 'ร้อนอะ':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='ลองหาอะไรเย็นๆกินดีมั้ย'))
+        return 0
+    if event.message.text == 'เบื่อมั้ย':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='ไม่เบื่อเลย เราชอบคุย'))
+        return 0
+    if event.message.text == 'ขอบคุณนะ':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='ยินดีค่ะ'))
+        return 0
+    if event.message.text == 'รู้ภาษาคนได้ไง':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='ก็ให้เธอช่วยคุยกับเราไง'))
+        return 0  
 
-if __name__ == '__main__':
-    app.run(debug = True)
+
+if __name__ == "__main__":
+    app.run()
